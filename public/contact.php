fe79880be5bef4d077cdfeba9af1fea75f4ee343
formdigital-cs <chris@form-digital.com>
2024-08-21 16:57:06 +0100

    Initial setup / header and footer markup

---
+++ public/contact.php
@@ -0,0 +1,10 @@
+<?php include "includes/header.php"; ?>
+
+<div class="px-1 py-4 text-center">
+    <h1 class="serif font-size-3xl uppercase">Get in touch</h1>
+    <div class="richtext">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat tellus neque, vitae faucibus sapien lacinia et. Vivamus volutpat pretium urna sit amet convallis. Aliquam molestie, ipsum quis ullamcorper accumsan, sapien augue porta odio, eu tincidunt purus urna sit amet erat.</p>
+    </div>
+</div>
+
+<?php include "includes/footer.php"; ?>
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php include "includes/header.php"; ?>
+        
+<h1>We are Wildings</h1>
+<p>We are Wildings</p>
+
+<?php include "includes/footer.php"; ?>
