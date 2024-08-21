6d356a630c48fb2f3087fe4e5175c5adb3775043
formdigital <kyle@form-digital.com>
2024-08-21 13:35:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# mix-webpack-starter
+
+### Setup
+
+Clone project
+```bash
+git clone git@github.com:formdigital/mix-webpack-starter.git
+```
+
+Open directory
+```bash
+cd mix-webpack-starter
+```
+
+Install dependancies
+```bash
+npm install
+```
+
+Watch assets for development
+```bash
+npm run watch
+```
+
+Build assets for production
+```bash
+npm run production
+```
---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+(self["webpackChunkmix_webpack_starter"] = self["webpackChunkmix_webpack_starter"] || []).push([["/js/app"],{
+
+/***/ "./src/js/app.js":
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
+/***/ (() => {
+
+// app scripts
+
+/***/ }),
+
+/***/ "./src/scss/app.scss":
+/*!***************************!*\
+  !*** ./src/scss/app.scss ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["css/app"], () => (__webpack_exec__("./src/js/app.js"), __webpack_exec__("./src/scss/app.scss")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
