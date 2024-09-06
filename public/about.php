038d5a6225f5ce1c44f1ad25ad776ea696841c1a
formdigital-cs <chris@form-digital.com>
2024-09-06 17:49:48 +0100

    About template markup WIP

---
+++ public/about.php
@@ -0,0 +1,49 @@
+<?php $page = "about"; $invertHeader = "true"; include "includes/header.php"; ?>
+
+<div class="hero heroLg">
+    <div class="heroMain px-1 py-5">
+        <h1 class="subheading uppercase font-size-sm fadeInReveal">Our Story</h1>
+        <h2 class="serif uppercase font-size-4xl text-block-10 fadeInReveal">Made in Lincolnshire</h2>
+    </div>
+    <div class="heroBg">
+        <img src="https://images.unsplash.com/photo-1595079917801-4a04e2985fba?q=80&w=4470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+    </div>
+</div>
+
+<div class="px-1 py-6">
+    
+</div>
+
+<div class="split bg-green">
+    <div class="splitText px-1 justify-between">
+        <div class="splitTextBlock">
+            <h2 class="serif uppercase font-size-2xl text-block-9 fadeInReveal">Committed to Sustainability</h2>
+        </div>
+        <div class="splitTextBlock">
+            <div class="richtext text-block-30 fadeInReveal">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat tellus neque, vitae faucibus sapien lacinia et. Vivamus volutpat pretium urna sit amet convallis. Aliquam molestie, ipsum quis ullamcorper accumsan, sapien augue porta odio, eu tincidunt purus urna sit amet erat.</p>
+            </div>
+        </div>
+    </div>
+    <div class="splitMedia parallaxContainer">
+        <img src="https://images.unsplash.com/photo-1593948379487-8c48a8470db6?q=80&w=4470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+    </div>
+</div>
+
+<div class="split splitReverse bg-yellow">
+    <div class="splitText px-1 justify-between">
+        <div class="splitTextBlock">
+            <h2 class="serif uppercase font-size-2xl text-block-9 fadeInReveal">Family</h2>
+        </div>
+        <div class="splitTextBlock">
+            <div class="richtext text-block-30 fadeInReveal">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat tellus neque, vitae faucibus sapien lacinia et. Vivamus volutpat pretium urna sit amet convallis. Aliquam molestie, ipsum quis ullamcorper accumsan, sapien augue porta odio, eu tincidunt purus urna sit amet erat.</p>
+            </div>
+        </div>
+    </div>
+    <div class="splitMedia parallaxContainer">
+        <img src="https://images.unsplash.com/photo-1593948379487-8c48a8470db6?q=80&w=4470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+    </div>
+</div>
+
+<?php include "includes/footer.php"; ?>
